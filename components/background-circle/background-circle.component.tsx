a99ef974a702030deaf056592a16af6f24a6d08b
LiangRVR <liangvillarrubia95@gmail.com>
2022-10-17 11:05:44 -0400

    refactor(responsive): make hero.component responsive

--- components/background-circle/background-circle.component.tsx
+++ components/background-circle/background-circle.component.tsx
@@ -18,8 +18,8 @@ const BackgroundCircle = () => {
       className="relative flex justify-center items-center"
     >{/* h-[200px] w-[200px] */}
       <div className="absolute border border-blue-400 rounded-full h-[35vw] w-[35vw] md:max-h-[200px] md:max-w-[200px] mt-52 animate-ping"/>
-      <div className="absolute border border-[#333333] rounded-full h-[55vw] w-[55vw] md:max-h-[300px] md:max-w-[300px] mt-52" />
-      <div className="absolute border border-[#333333] rounded-full h-[75vw] w-[75vw] md:max-h-[500px] md:max-w-[500px] mt-52" />
+      <div className="absolute border border-[#333333] rounded-full h-[45vw] w-[45vw] md:max-h-[300px] md:max-w-[300px] mt-52" />
+      <div className="absolute border border-[#333333] rounded-full h-[65vw] w-[65vw] md:max-h-[500px] md:max-w-[500px] mt-52" />
       <div className="absolute border border-[#F7Ab0A] opacity-20 rounded-full h-[85vw] w-[85vw] md:max-h-[650px] md:max-w-[650px] mt-52 animate-pulse" />
       <div className="absolute border border-[#333333] rounded-full h-[95vw] w-[95vw] md:max-h-[800px] md:max-w-[800px] mt-52 " />
     </motion.div>
--- components/hero/hero.component.tsx
+++ components/hero/hero.component.tsx
@@ -14,34 +14,33 @@ const Hero = () => {
       "<AlwaysCoding props={amazingProjects} />",
     ],
     loop: true,
-    delaySpeed: 2000,
+    delaySpeed: 3000,
   });
   return (
     <div className="h-screen flex flex-col space-y-8 justify-center items-center text-center overflow-hidden">
       <BackgroundCircle />
-      <Image
-        src={
-          "https://drive.google.com/uc?export=view&id=19XhMPQzcyChRgqsPsrpHh5Ki6y0Aia0b"
-        }
-        alt="Liang`s picture"
-        height={"128px"}
-        width={"128px"}
-        objectFit="cover"
-        className="relative rounded-full mx-auto"
-      />
+      <div className="relative mx-auto h-[20vw] w-[20vw] md:h-32 md:w-32">
+        <Image
+          src="https://drive.google.com/uc?export=view&id=19XhMPQzcyChRgqsPsrpHh5Ki6y0Aia0b"
+          alt="Liang`s picture"
+          objectFit="cover"
+          layout="fill"
+          className="rounded-full"
+        />
+      </div>
 
       <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[10px]">
+        <h2 className="text-xs md:text-sm lg:text-lg uppercase text-gray-500 pb-2 tracking-[10px]">
           Full Stack Developer
         </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-semibold px-10">
           <span>{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
         <div className="pt-5">
           <HeroButton text={"About"} href="#about" />
           <HeroButton text={"Experience"} href="#experience" />
-          <HeroButton text={"Skills"}  href="#skills"/>
+          <HeroButton text={"Skills"} href="#skills" />
           <HeroButton text={"Projects"} href="#projects" />
         </div>
       </div>
